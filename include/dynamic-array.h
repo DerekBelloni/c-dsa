fdb803ede5594935a885af2ae1ce4d9520b12cd8
Derek Belloni <derekbelloni10@gmail.com>
2025-02-21 23:38:28 -0700

    started outline function sigs for dynamic array

--- include/dynamic-array.h
+++ include/dynamic-array.h
@@ -1,7 +1,10 @@
 #ifndef DYNAMIC_ARRAY_H
 #define DYNAMIC_ARRAY_H
 
-
-
+int getValue(int index);
+int push(int value);
+int pop(int value);
+int insertAtIndex(int value, int index);
+int resize(int size);
 
 #endif
